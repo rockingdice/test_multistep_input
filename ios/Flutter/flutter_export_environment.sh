18511eed49f7423f5a325945495d53921601b9b3
RED <rockingdice@gmail.com>
2019-09-19 16:50:28 +0800

    init

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/li9s/github/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/li9s/github/flutter_projects/test_multistep_input"
+export "FLUTTER_TARGET=/Users/li9s/github/flutter_projects/test_multistep_input/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/li9s/github/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
